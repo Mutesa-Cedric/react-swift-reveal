8d53ecaa3038ab9e9c4161492ba9712aa2e5cced
Mutesa-Cedric <mutesacedric@gmail.com>
2023-09-27 18:08:35 +0200

    init(docs) : setup documentation site

---
+++ apps/docs/README.md
@@ -0,0 +1,3 @@
+# React-swiftreveal Docs.
+
+React-swiftreveal is a React component library and animation framework for animating elements as they enter the viewport.
---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+import Head from 'next/head'
+
+const config: DocsThemeConfig = {
+  logo: <span>React-swiftreveal</span>,
+  project: {
+    link: 'https://github.com/Mutesa-Cedric/react-swiftReveal',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/Mutesa-Cedric/react-swiftReveal/tree/main/apps/docs',
+  footer: {
+    text: 'Made with love by Mutesa Cedric.',
+  },
+}
+
+export default config
