8bf83203a24c21b26fccbb20b124184701abe79f
Mutesa-Cedric <mutesacedric@gmail.com>
2023-09-27 16:56:21 +0200

    init(examples) : setup for example app

---
+++ apps/examples/README.md
@@ -0,0 +1,3 @@
+# React-swiftreveal examples
+
+examples of how to use different animations provided by react-swiftreveal.
---
+++ apps/examples/src/App.tsx
@@ -0,0 +1,15 @@
+import { Flip } from '../../../library/'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Flip>
+        <h1>Hi there </h1>
+      </Flip>
+    </>
+  )
+}
+
+export default App
